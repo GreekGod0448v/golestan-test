a35ad7f7e385b291047a8c5e26677aabc04177b8
GreekGod0448v <you@example.com>
2023-12-14 18:39:58 +0330

    cypress settings is add

---
+++ features/step_definitions/stepdefs.spec.js
@@ -0,0 +1,22 @@
+const {When , Given , Then} = require( '@cucumber/cucumber')
+
+When('send request to the my signin api', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+});
+
+
+Given('have code and password as <data>', function (data) {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+});
+
+Then('you should return to me my <answer>', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+});
+
+Then('you should return to me my <fullnama>', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+});
